6838898e5745a5414b9e1341985c4c3e5c035223
JCarlos <jcreyes@generagames.com>
2017-05-29 21:43:57 +0200

    first script

---
+++ Assets/Scripts/CharacterAnimationController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationController : MonoBehaviour {
+
+    public float accel = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x + 0.25f, transform.position.y, transform.position.z), Time.time * Time.deltaTime * accel);
+	}
+}
